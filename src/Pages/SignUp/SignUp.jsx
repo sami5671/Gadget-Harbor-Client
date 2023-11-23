0ad880db518441ea1a18cfe9e4fb8f4b503649c6
sami5671 <samialam5671@gmail.com>
2023-11-24 03:38:48 +0600

    3. added the Sign Up page and create account on firebase

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,15 +1,127 @@
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import registerImg from "../../../src/assets/Images/registerr.jpg";
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const SignUp = () => {
-  // ==================================================================
-
+  const { createUser } = useContext(AuthContext);
+  // ======================react hook form============================================
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm();
   // =================================================================
+
+  const onSubmit = (data) => {
+    console.log(data);
+    createUser(data.email, data.password).then((result) => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+    });
+  };
+
+  //================================================================
   return (
     <div>
       <Helmet>
         <title>Gadget Harbor || SignUp</title>
       </Helmet>
-      <h1>this is register page</h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <img className="w-[600px]" src={registerImg} alt="" />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("name", { required: true })}
+                  placeholder="name"
+                  className="input input-bordered"
+                  required
+                />
+                {errors.name && (
+                  <span className="text-red-400">name field is required</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo Url</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("photoURL", { required: true })}
+                  placeholder="photo URL"
+                  className="input input-bordered"
+                  required
+                />
+                {errors.photoURL && (
+                  <span className="text-red-400">photoURL is required</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  {...register("email", { required: true })}
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+                {errors.email && (
+                  <span className="text-red-400">Email field is required</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  {...register("password", { required: true })}
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                {errors.password && (
+                  <span className="text-red-400">
+                    Password field is required
+                  </span>
+                )}
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Sign Up"
+                />
+              </div>
+            </form>
+            {/* <SocialLogin></SocialLogin> */}
+            <p className="px-8">
+              <small>
+                <Link to="/login"> Already have an account? Login Here...</Link>
+              </small>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
