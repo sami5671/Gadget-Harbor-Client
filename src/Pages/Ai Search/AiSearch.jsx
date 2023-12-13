fc8d4d2e34de54daf586ab3a6d39e46ed6d7d77c
sami5671 <samialam5671@gmail.com>
2023-12-13 13:43:54 +0600

    added samgpt

--- src/Pages/Ai Search/AiSearch.jsx
+++ src/Pages/Ai Search/AiSearch.jsx
@@ -10,12 +10,14 @@ import {
 } from "@chatscope/chat-ui-kit-react";
 import { useState } from "react";
 
+const API_KEY = "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";
 const AiSearch = () => {
+  const [typing, setTyping] = useState(false);
   // ==========
   const [messages, setMessages] = useState([
     {
       message: "Hello, I am  samGpt!",
-      sender: "ChatGpt",
+      sender: "ChatGPT",
     },
   ]);
 
@@ -26,12 +28,52 @@ const AiSearch = () => {
       direction: "outgoing",
     };
 
-    const newMessages = [...messages, newMessages]; //all new messages +old messages
+    const newMessages = [...messages, newMessage]; //all new messages +old messages
 
     // update our message state
-    setMessages(newMessage);
+    setMessages(newMessages);
+
+    // typing indicator (samGPT is typing)
+    setTyping(true);
     // process the message to chatgpt
+    await processMessageToChatgpt(newMessages);
   };
+  async function processMessageToChatgpt(chatMessages) {
+    // =============
+    // =============================
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message };
+    });
+
+    const systemMessage = {
+      role: "system",
+      content: "Explain all concepts like I am 10 years old.",
+    };
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, ...apiMessages],
+    };
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }
   // =========
   return (
     <div>
@@ -42,11 +84,19 @@ const AiSearch = () => {
       ></SectionTitle>
 
       {/* ========== */}
-      <div>
+      <div className="App">
         <div style={{ position: "relative", height: "400px", width: "700px" }}>
           <MainContainer>
             <ChatContainer>
-              <MessageList>
+              <MessageList
+                typingIndicator={
+                  typing ? (
+                    <>
+                      <TypingIndicator content="SamGPT is Typing" />{" "}
+                    </>
+                  ) : null
+                }
+              >
                 {messages.map((message, i) => {
                   return <Message key={i} model={message}></Message>;
                 })}
